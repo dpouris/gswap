a53e3c4884eeb2ab1b967ab311cf5d7cab0f4ef2
Dpouris <jimpouris0@gmail.com>
2022-05-15 17:40:30 +0300

    Fixed bug with index

--- dist/src/main.d.ts
+++ dist/src/main.d.ts
@@ -1,15 +1,11 @@
 import type { Options, GallerySwap } from "../@types/GallerySwapTypes";
 export declare class GSwap implements GallerySwap {
-  #private;
-  containerElem: HTMLDivElement;
-  images: string[];
-  options: Options;
-  constructor(
-    containerElem: string | HTMLDivElement,
-    images: string[],
-    options?: Options
-  );
-  stackImages: () => void;
-  next: () => void;
-  prev: () => void;
+    #private;
+    containerElem: HTMLDivElement;
+    images: string[];
+    options: Options;
+    constructor(containerElem: string | HTMLDivElement, images: string[], options?: Options);
+    stackImages: () => void;
+    next: () => void;
+    prev: () => void;
 }
